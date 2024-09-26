107eae8b60ded826af16fb4a0e36b11017207b3f
Peng <sp100809@gmail.com>
2024-09-27 08:25:31 +1000

    this time might be working?

--- start.sh
+++ start.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-# Ensure the docker group exists
-if ! getent group docker > /dev/null 2>&1; then
-    sudo groupadd docker
-fi
-
 check_success() {
   if [ $? -ne 0 ]; then
       echo "Error: $1"
       exit 1
   fi
 }
-
-DOCKER_GID=$(getent group docker | cut -d: -f3)
 
 docker build -t shaun/jenkins:1.0.0 .
 check_success "failed to build jenkins docker image"
@@ -27,6 +20,5 @@ docker run -d \
     -e CASC_JENKINS_CONFIG=/var/jenkins_home/jenkins.yaml \
     -e DOCKER_HOST=unix:///var/run/docker.sock \
     -e JAVA_OPTS="-Djenkins.install.runSetupWizard=false -Djenkins.model.Jenkins.slaveAgentPort=50000 -Dhudson.TcpSlaveAgentListener.hostName=myjenkins.loca.lt" \
-    --group-add ${DOCKER_GID} \
     --restart always \
     shaun/jenkins:1.0.0
