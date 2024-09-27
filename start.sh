90a6953c0a17e78fe49b6ad84d6c6697805bd3f0
Peng <sp100809@gmail.com>
2024-09-28 01:31:37 +1000

    1

--- start.sh
+++ start.sh
@@ -53,7 +53,6 @@ check_success "failed to build jenkins docker image"
 
 docker run -d \
     --name jenkins \
-    # --privileged \
     -p 8080:8080 -p 50000:50000 \
     -v jenkins_home:/var/jenkins_home \
     -v /var/run/docker.sock:/var/run/docker.sock \
